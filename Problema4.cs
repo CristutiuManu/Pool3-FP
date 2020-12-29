93378c383b5239cdb8e0e1c55b46592aaca16462
Cristutiu Emanuel <cristutiumanu@gmail.com>
2020-12-29 21:29:02 +0200

    Added Problem 5

--- Problema4.cs
+++ Problema4.cs
@@ -3,18 +3,24 @@ namespace Pool3
 {
     public class Problema4
     {
-        // Deteminati printr-o singura parcurgere, cea mai mica si cea mai mare valoare dintr-un vector si de cate ori apar acestea. 
+        // Deteminati printr-o singura parcurgere, cea mai mica si cea mai mare valoare dintr-un vector si de cate ori apar acestea.
+
         public Problema4()
         {
             Console.WriteLine("Introduceti N-ul");
             int n = int.Parse(Console.ReadLine());
-            Random rnd = new Random();
             int[] v = new int[n];
+            int aparitiiMinim = 0;
+            int aparitiiMaxim = 0;
+            int contorTotal = 0;
 
             for (int i = 0; i < n; i++)
             {
-                v[i] = rnd.Next(99);
+                v[i] = int.Parse(Console.ReadLine());
             }
+
+            int maxim = v[0];
+            int minim = v[0];
 
             Console.WriteLine("Vectorul citit este: ");
             for (int i = 0; i < n; i++)
@@ -22,29 +28,28 @@ namespace Pool3
 
             Console.WriteLine();
 
-            int maxim = v[0];
-            int minim = v[0];
-            int contorMinim = 0;
-            int contorMaxim = 0;
-            int contorTotal = 0;
             for(int i = 0; i < n; i++)
             {
-                 if (maxim < v[i])
+                if (v[i] > maxim)
+                {
                     maxim = v[i];
+                    aparitiiMaxim = 1;
+                }
+                else if (v[i] == maxim)
+                    aparitiiMaxim++;
 
-                 if (minim > v[i])
+                if (minim > v[i])
+                {
                     minim = v[i];
-
-               // De revizuit
-
-
+                    aparitiiMinim = 1;
+                }
+                else if (v[i] == minim)
+                    aparitiiMinim++;
             }
-            contorTotal = contorMinim + contorMaxim;
-            Console.WriteLine($"Maximul este {maxim}, minimul este {minim}, iar numarul de aparitii pentru maxim si minim este: {contorMaxim}, respectiv {contorMinim}");
-            Console.WriteLine($"Numarul total de aparitii al celor doua este: {contorTotal}");
-            
 
-            
+            contorTotal = aparitiiMinim + aparitiiMaxim;
+            Console.WriteLine($"Maximul este {maxim}, minimul este {minim}, iar numarul de aparitii pentru maxim si minim este: {aparitiiMaxim}, respectiv {aparitiiMinim}");
+            Console.WriteLine($"Numarul total de aparitii al celor doua este: {contorTotal}");
         }
     }
 }
---
+++ Problema5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace Pool3
+{
+    public class Problema5
+    {
+        // Se da un vector cu n elemente, o valoare e si o pozitie din vector k.
+        // Se cere sa se insereze valoarea e in vector pe pozitia k. Primul element al vectorului se considera pe pozitia zero. 
+        public Problema5()
+        {
+            Console.WriteLine("Introduceti N-ul");
+            int n = int.Parse(Console.ReadLine());
+            int e;
+            int k;
+            Random random = new Random();
+            int[] v = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = random.Next(100);
+            }
+
+            Console.WriteLine("Vectorul citit este: ");
+            for (int i = 0; i < n; i++)
+                Console.Write(v[i] + " ");
+
+            Console.WriteLine();
+            Console.WriteLine("Introdu valoarea e: ");
+            e = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu pozitia unde se va afla valoarea e");
+            k = int.Parse(Console.ReadLine());
+
+           
+            for (int i = 0; i < k; i++)
+            {
+                v[i] = v[i + 1];
+            }
+            n++;
+
+            v[k] = e;
+
+            for (int i = 0; i < n; i++)
+                Console.Write(v[i] + " ");
+
+        }
+    }
+}
