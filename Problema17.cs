670ffdb3d9fa59301c566a137190b6050cd05e68
Cristutiu Emanuel <cristutiumanu@gmail.com>
2021-01-06 18:12:05 +0200

    Fixed problem 17

--- Problema17.cs
+++ Problema17.cs
@@ -6,21 +6,35 @@ namespace Pool3
         // Se da un numar n in baza 10 si un numar b. 1 < b < 17. Sa se converteasca si sa se afiseze numarul n in baza b.   
         public Problema17()
         {
+            int i, j, k;
             Console.WriteLine("Introduceti N-ul");
             int n = int.Parse(Console.ReadLine());
-            int[] v = new int[n];
-            Random random = new Random();
 
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Introduceti baza ls care doriti sa se converteasca numarul");
+
+            int b = int.Parse(Console.ReadLine());
+
+            int conversieInitiala = 0;
+            
+            while (n % b != 0)
             {
-                v[i] = random.Next(50);
+                int rest = n % b;
+                conversieInitiala += rest;
+                conversieInitiala *= 10;
+                n = n / b;
             }
 
-            for (int i = 0; i < n; i++)
+            if (conversieInitiala % 10 == 0)
+                conversieInitiala /= 10;
+
+            int conversieFinala = 0;
+            while (conversieInitiala != 0)
             {
-                Console.Write(v[i] + " ");
+                conversieFinala = conversieFinala * 10 + conversieInitiala % 10;
+                conversieInitiala /= 10;
             }
-            Console.WriteLine();
+
+            Console.WriteLine("Numarul convertit este egal cu: " + conversieFinala);
 
 
 
